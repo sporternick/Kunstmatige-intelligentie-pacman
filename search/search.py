b8899fb30426a5b5e853b059e267c8a27dfc2404
Lukas van der Lans <lukasvdl10@gmail.com>
2018-11-23 16:43:45 +0100

    Implemented uniform cost search
    Changed the Queue to a PriorityQueue which uses the given cost value

--- search/search.py
+++ search/search.py
@@ -155,7 +155,33 @@ def breadthFirstSearch(problem):
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    #Add starting state to stack
+    frontier = util.PriorityQueue()
+    startState = problem.getStartState()
+    frontier.push(startState+([],),0)
+
+    #Frontier
+    visitedNodes = []
+
+    #While moves are avaible
+    while(not frontier.isEmpty()):
+        #Get next move in frontier and add to visisted nodes
+        state = frontier.pop()
+        visitedNodes.append((state[0],state[1]))
+
+        #Return path if goal found
+        if(problem.isGoalState((state[0],state[1]))):
+            return state[2]
+        #Add succesors to list
+        else:
+            successors = problem.getSuccessors((state[0],state[1]))
+            for s in successors:
+                succ = s[0]
+                if(succ not in visitedNodes):
+                    #Add new move to path
+                    path = state[2]+[s[1]]
+                    #Push succesor onto stack with updated path
+                    frontier.push(succ+(path,),s[2])
 
 def nullHeuristic(state, problem=None):
     """
